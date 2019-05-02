04e02f2f92a910b82e22a79bba603026b9227bde
Piotr Kowalski <piecioshka@gmail.com>
2019-05-02 13:33:24 +0200

    Clean test directory

--- test/test.index.js
+++ test/test.index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const { attack } = require('../../../');
+const { attack } = require('..');
 const HTTPStatusCodes = require('http-status-codes');
 
 const EXAMPLE_URL = 'http://example.org';
